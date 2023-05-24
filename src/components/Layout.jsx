0d12a3b2b7e7cc4b795a8992c8c057d8269d776e
BoltUltra <mayowa.u.sunusi@gmail.com>
2023-05-24 08:08:41 +0100

    God of bug, please help

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-export default function Layout({ children }) {
+const Layout = ({children}) => {
   return (
-    <>
+     <>
       <Navbar />
       <main>{children}</main>
       <Footer />
     </>
-  );
+  )
 }
+
+export default Layout
